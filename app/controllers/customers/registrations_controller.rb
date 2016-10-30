a390c588129e6eb24f9fa1f58aa1230dc67160cc
Kori Roys <kori@koriroys.com>
2016-10-30 17:57:55 +0100

    move fake order generation to happen in controller after user registration - removes model callback

---
+++ app/controllers/customers/registrations_controller.rb
@@ -0,0 +1,74 @@
+class Customers::RegistrationsController < Devise::RegistrationsController
+# before_action :configure_sign_up_params, only: [:create]
+# before_action :configure_account_update_params, only: [:update]
+
+  # GET /resource/sign_up
+  # def new
+  #   super
+  # end
+
+  # POST /resource
+  def create
+    super do |customer|
+      generate_fake_orders(customer)
+    end
+  end
+
+  def generate_fake_orders(customer)
+    product_ids = Product.pluck(:id)
+    5.times do
+      timestamp = Faker::Time.between(2.weeks.ago, DateTime.now)
+      customer.orders.create!(
+        product_id: product_ids.sample,
+        status: Order.statuses.values.sample,
+        created_at: timestamp,
+        updated_at: timestamp)
+    end
+  end
+
+  # GET /resource/edit
+  # def edit
+  #   super
+  # end
+
+  # PUT /resource
+  # def update
+  #   super
+  # end
+
+  # DELETE /resource
+  # def destroy
+  #   super
+  # end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  # end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  # end
+
+  # The path used after sign up.
+  # def after_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+
+  # The path used after sign up for inactive accounts.
+  # def after_inactive_sign_up_path_for(resource)
+  #   super(resource)
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :customers
+  devise_for :customers, controllers: { registrations: "customers/registrations" }
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
