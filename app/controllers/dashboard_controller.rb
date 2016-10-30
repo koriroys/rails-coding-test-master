c106ff95fe03a29365e62bfc1776ba24333e6068
Kori Roys <kori@koriroys.com>
2016-10-30 18:05:01 +0100

    move database logic to model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,8 +7,7 @@ class DashboardController < ApplicationController
     # 1. Breakdown by product of sold quantities (based on orders.created_at)
     date = params[:date]
     @day = Date.parse(date)
-    @products_sold_count = Order.joins(:product).from_week_starting_on(@day).group(:product).count
-
+    @products_sold_count = Order.sold_count_for_week_starting_on(@day)
 
     # 2. Breakdown by items of sold quantities (based on orders.created_at)
     items_sold_count = Order.includes(:items)
@@ -28,11 +27,18 @@ class DashboardController < ApplicationController
 
     # 4. Display order uniq customer count by number of orders (example 1)
     # Order.joins(:customer).group(:customer).count
-    orders_by_customer = Order.joins(:customer).group(:customer_id).count
+    orders_by_customer = Order.joins(:customer)
+      .from_week_starting_on(@day)
+      .group(:customer_id)
+      .count
 
     # key will be number of orders, value will be how many customers ordered (key) times
     # e.g. { 9 => 3} means three customers each had 9 orders
-    @orders_count = orders_by_customer.values.each.with_object(Hash.new(0)) { |order_count, counts| counts[order_count] += 1 }
+    @orders_count = orders_by_customer.values
+      .each
+      .with_object(Hash.new(0)) do |order_count, counts|
+        counts[order_count] += 1
+      end
     @total_orders = @orders_count.values.sum
 
     # 5. (*On a separate view*) Display repartition between reccuring and new customers for each month (example 2)
--- app/models/order.rb
+++ app/models/order.rb
@@ -5,5 +5,13 @@ class Order < ActiveRecord::Base
 
   scope :from_week_starting_on, -> (start_date) { where(created_at: start_date..(start_date + 7.days))}
 
+  scope :sold_count_for_week_starting_on, -> (start_date) {
+    joins(:product)
+      .from_week_starting_on(start_date)
+      .group(:product)
+      .count
+  }
+
+
   enum status: [:draft, :confirmed, :canceled]
 end
